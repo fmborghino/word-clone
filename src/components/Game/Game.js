33449671232ace8ec48b3f32a777432c53455f3a
Max Borghino <fmborghino@gmail.com>
2023-02-20 22:47:36 -0800

    Add input form (ex 1)

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,15 +1,38 @@
 import React from 'react';
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import {sample} from '../../utils';
+import {WORDS} from '../../data';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
+console.info({answer});
 
 function Game() {
-  return <>Put a game here!</>;
+    const [guess, setGuess] = React.useState('');
+    return (
+        <>
+            <form className="guess-input-wrapper"
+                  onSubmit={(e) => {
+                      e.preventDefault();
+                      setGuess('');
+                      console.log({guess});
+                  }}>
+                <label htmlFor="guess-input">Enter guess:</label>
+                <input
+                    id="guess-input"
+                    type="text"
+                    value={guess}
+                    onChange={(e) => {
+                        guess.length <= 5 && setGuess(e.target.value.toUpperCase())
+                    }}
+                    required={true}
+                    maxLength={5}
+                    pattern={'[A-Z]{5}'}
+                    // placeholder={'— — — — —'}
+                />
+            </form>
+        </>);
 }
 
 export default Game;
